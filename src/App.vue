4460f0b5a8f53bb8625e21a3eca7c1f19ae8d34b
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2024-04-12 16:03:10 +0200

    installation du v-show sur la nav

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,7 @@ const menuIsOpen = ref(false)
     <button aria-controls="mainNav" aria-expanded="true" class="rounded-full border-2 border-red-600 bg-red-300 px-2">
       menu
     </button>
-    <nav id="mainNav">
+    <nav v-show="menuIsOpen" id="mainNav">
       <ul>
         <li>
           <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>

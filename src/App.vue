c9973a3934dd8dcd542965f945238d0150f92b64
Gachod-Pierre <pierre_jimmy_laurent.gachod-martin@edu.univ-fcomte.fr>
2024-04-12 16:08:41 +0200

    ajout de la transition autour de la nav

--- src/App.vue
+++ src/App.vue
@@ -17,16 +17,19 @@ const menuIsOpen = ref(false)
       class="rounded-full border-2 border-red-600 bg-red-300 px-2">
       menu
     </button>
-    <nav v-show="menuIsOpen" id="mainNav">
-      <ul>
-        <li>
-          <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
-        </li>
-        <li>
-          <RouterLink to="/accordeon" class="text-red-500 underline"> accordeon </RouterLink>
-        </li>
-      </ul>
-    </nav>
+    <Transition class="transition-transform duration-300" enter-from-class="-translate-x-full"
+      enter-to-class="translate-x-0" leave-active-class="-translate-x-full">
+      <nav v-show="menuIsOpen" id="mainNav">
+        <ul>
+          <li>
+            <RouterLink to="/" class="text-red-500 underline"> Accueil </RouterLink>
+          </li>
+          <li>
+            <RouterLink to="/accordeon" class="text-red-500 underline"> accordeon </RouterLink>
+          </li>
+        </ul>
+      </nav>
+    </Transition>
   </header>
   <RouterView v-slot="{ Component }">
     <Suspense>
